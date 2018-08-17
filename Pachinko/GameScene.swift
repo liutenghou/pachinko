720b9a35a6b5374029ebbf693f0a4101bcb8e5e2
Leo Liu <leo.liu@aequilibrium.ca>
2018-08-17 13:57:33 -0700

    slot glow spin added

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -20,11 +20,16 @@ class GameScene: SKScene {
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
-        makeBouncer(at: CGPoint(x: 0, y: 0))
-        makeBouncer(at: CGPoint(x: 256, y: 0))
-        makeBouncer(at: CGPoint(x: 512, y: 0))
-        makeBouncer(at: CGPoint(x: 768, y: 0))
-        makeBouncer(at: CGPoint(x: 1024, y: 0))
+        placeBouncer(at: CGPoint(x: 0, y: 0))
+        placeBouncer(at: CGPoint(x: 256, y: 0))
+        placeBouncer(at: CGPoint(x: 512, y: 0))
+        placeBouncer(at: CGPoint(x: 768, y: 0))
+        placeBouncer(at: CGPoint(x: 1024, y: 0))
+        
+        placeSlot(at: CGPoint(x:128, y:0), isGood: true)
+        placeSlot(at: CGPoint(x:384, y:0), isGood: false)
+        placeSlot(at: CGPoint(x:640, y:0), isGood: true)
+        placeSlot(at: CGPoint(x:896, y:0), isGood: false)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -39,11 +44,33 @@ class GameScene: SKScene {
     }
     
     //MARK: helpers
-    func makeBouncer(at position: CGPoint) {
+    func placeBouncer(at position: CGPoint) {
         let bouncer = SKSpriteNode(imageNamed: "bouncer")
         bouncer.position = position
         bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
         bouncer.physicsBody?.isDynamic = false
         addChild(bouncer)
+    }
+    
+    func placeSlot(at position: CGPoint, isGood: Bool) {
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+        
+        if isGood {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+        } else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+        }
+        
+        slotBase.position = position
+        slotGlow.position = position
+        addChild(slotBase)
+        addChild(slotGlow)
+        
+        let spinGlow = SKAction.rotate(byAngle: CGFloat.pi, duration: 10)
+        let spinGlowForever = SKAction.repeatForever(spinGlow)
+        slotGlow.run(spinGlowForever)
     }
 }
